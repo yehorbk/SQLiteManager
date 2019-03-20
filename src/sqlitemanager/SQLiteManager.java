eabf186708ff653edcdb1c0994786877276268d5
George Bublyk <e.bublyk1010@gmail.com>
2019-03-20 16:39:06 +0200

    MainWindow controller updated: setStage method added. Main Apllication class updated: start controller functionality added.

--- src/sqlitemanager/SQLiteManager.java
+++ src/sqlitemanager/SQLiteManager.java
@@ -1,42 +1,51 @@
 
 package sqlitemanager;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import sqlitemanager.controller.MainWindowController;
 
 
 public class SQLiteManager extends Application {
     
+    private Stage primaryStage;
+    
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        try {
             
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        this.primaryStage = primaryStage;
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("ui/MainWindow.fxml"));
+        Parent root = loader.load();
+        MainWindowController controller = loader.getController();
         
-        Scene scene = new Scene(root, 300, 250);
+        controller.setStage(primaryStage);
+        Scene scene = new Scene(root);
         
-        primaryStage.setTitle("Hello World!");
+        scene.getStylesheets().clear();
+        scene.getStylesheets().add(this.getClass().getResource("style/mainwindow.css").toExternalForm());
+        
+        primaryStage.setTitle("SQLite Manager");
         primaryStage.setScene(scene);
+        primaryStage.setFullScreenExitHint("");
         primaryStage.show();
+        
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
+    
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
--- src/sqlitemanager/controller/MainWindowController.java
+++ src/sqlitemanager/controller/MainWindowController.java
@@ -1,27 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sqlitemanager.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author User
- */
 public class MainWindowController implements Initializable {
+    
+    private Stage stage;
 
-    /**
-     * Initializes the controller class.
-     */
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }
